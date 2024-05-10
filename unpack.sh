e4eb9da2cb6bb2a0ee77b241548bd03e843e9f38
mgood7123 <smallville7123@gmail.com>
2024-05-10 21:07:07 +1000

    Update unpack.sh

--- unpack.sh
+++ unpack.sh
@@ -1,3 +1,3 @@
 cd .
 
-./split_exe/split.exe --join VirtualBox-7.0.18.split/VirtualBox-7.0.18.split.map --out VirtualBox-7.0.18 -r -v
+./split_exe/split.exe --join VirtualBox-7.0.18.split/VirtualBox-7.0.18.split.map --out VirtualBox-7.0.18 -r
